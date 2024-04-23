933e364a5566f9003254b0c79df3bff074daa417
bluefield <59333909+bluefieldcreator@users.noreply.github.com>
2024-04-23 08:37:05 +0200

    feat: update constants

--- Projects/led_counter.cpp
+++ Projects/led_counter.cpp
@@ -1,26 +1,22 @@
-#define wait_time 500
-
-int pines[] = { 13, 12, 11, 10, 9 };
+const int wait_time = 500;
+const int pines[] = {13, 12, 11, 10, 9};
+const int numPins = sizeof(pines) / sizeof(pines[0]);
 
 void setup() {
-  int length = sizeof(pines) / sizeof(pines[0]);
-  for (int n = 0; n < length; n++) {
+  for (int n = 0; n < numPins; n++) {
     pinMode(pines[n], OUTPUT);
   }
 }
 
 void setAll(int state) {
-  int length = sizeof(pines) / sizeof(pines[0]);
-  for (int n = 0; n < length; n++) {
+  for (int n = 0; n < numPins; n++) {
     digitalWrite(pines[n], state);
   }
   delay(wait_time);
 }
 
 void reverseLoop() {
-  int length = sizeof(pines) / sizeof(pines[0]);
-
-  for (int i = length - 1; i >= 0; i--) {
+  for (int i = numPins - 1; i >= 0; i--) {
     digitalWrite(pines[i], 1);
     delay(wait_time);
     digitalWrite(pines[i - 1], 0);
@@ -29,15 +25,14 @@ void reverseLoop() {
 }
 
 void forwardLoop() {
-  int length = sizeof(pines) / sizeof(pines[0]);
-
-  for (int n = 0; n < length; n++) {
+  for (int n = 0; n < numPins; n++) {
     digitalWrite(pines[n], 1);
-    delay(200);
+    delay(wait_time);
     digitalWrite(pines[n + 1], 0);
     delay(wait_time);
   }
 }
+
 void loop() {
   forwardLoop();
   setAll(0);
